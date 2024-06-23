09c5783d82c9c1477c18bb1895acd2a3c34508dd
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-06-23 10:54:29 +0200

    Base textnode class

---
+++ src/textnode.py
@@ -0,0 +1,13 @@
+class TextNode:
+    """A markdown textnode"""
+
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, cmp):
+        return self.text == cmp.text and self.text_type == cmp.text_type and self.url == cmp.url
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
