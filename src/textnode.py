7594588a2535a57b154e8f9a54ac56d0737f863e
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-07-08 17:30:24 +0200

    Fix broken indent
    Dang you auto indent!

--- src/textnode.py
+++ src/textnode.py
@@ -25,19 +25,19 @@ class TextNode:
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
     
-    def text_node_to_hmtl_node(text_node):
-        if text_node.text_type == type_text:
-            return LeafNode(None, text_node.text)
-        if text_node.text_type == type_bold:
-            return LeafNode('b', text_node.text)
-        if text_node.text_type == type_italic:
-            return LeafNOde('i', text_node.text)
-        if text_node.text_type == type_code:
-            return LeafNode('code', text_node.text)
-        if text_node.text_type == type_link:
-            return LeafNode('a', text_node.text, {"href": text_node.url})
-        if text_node.text_type == type_image:
-            return LeafNode('img', '', {"alt": text_node.text, "src": text_node.url})
-        
-        raise ValueError(f"Unsupported text type: {text_node.text_type}")
+def text_node_to_html_node(text_node):
+    if text_node.text_type == type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == type_bold:
+        return LeafNode('b', text_node.text)
+    if text_node.text_type == type_italic:
+        return LeafNode('i', text_node.text)
+    if text_node.text_type == type_code:
+        return LeafNode('code', text_node.text)
+    if text_node.text_type == type_link:
+        return LeafNode('a', text_node.text, {"href": text_node.url})
+    if text_node.text_type == type_image:
+        return LeafNode('img', '', {"alt": text_node.text, "src": text_node.url})
+    
+    raise ValueError(f"Unsupported text type: {text_node.text_type}")
 
