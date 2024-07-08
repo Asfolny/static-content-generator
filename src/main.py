9a65ab425ef0115e1c5b10a21dcaa088287d2a80
Rasmus Frederiksen <rasmus@ufafarion.com>
2024-07-08 19:27:59 +0200

    Use static copy

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
-from textnode import TextNode
-from htmlnode import LeafNode
+import os
+import shutil
+from static import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    print(TextNode("This is a text node", "bold", "https://www.boot.dev"))
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 
 main()
+
